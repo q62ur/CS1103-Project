f4cf3029efb60495d9a8ebb77be1af809d5e8324
q62ur <q62ur@unb.ca>
2026-03-19 21:30:32 -0300

    Create DatabaseSetup.java

---
+++ code/code/DatabaseSetup.java
@@ -0,0 +1,17 @@
+//import required JDBC classes
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+//main class to setup database
+public class DatabaseSetup 
+{
+
+    //main method where program starts
+    public static void main(String[] args) 
+  {
+
+        //simple message to confirm program runs
+        System.out.println("Database setup file created.");
+    }
+}
